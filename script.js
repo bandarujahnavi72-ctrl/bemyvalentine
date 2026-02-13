835ac7c1186911d5a2b0a83853fe9d5b1975e386
bandarujahnavi72-ctrl <bandarujahnavi72@gmail.com>
2026-02-13 22:08:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 300;
+  const y = Math.random() * 300;
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+  message.innerHTML = "Yayyyy!! 💕 I knew it 😍";
+});
